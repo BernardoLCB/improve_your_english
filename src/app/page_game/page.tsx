ffa6974321d42f271bcc9bfec82cccec76c9f95c
bernardoleinig <bernardoleinig@gmail.com>
2025-08-01 00:34:01 -0300

    inicializado o layout da página page_game baseado no jogo do milhão

--- src/app/page_game/page.tsx
+++ src/app/page_game/page.tsx
@@ -6,6 +6,17 @@ import { useState } from "react";
 //            <button onClick = {botao} className="text-2xl hover:cursor-pointer transition hover:scale-105 bg-blue-800 p-3 //rounded-2xl">conteudo - {valor}</button>
 //        </section>
 
+type prop = {
+    possible_answer:string,
+    number:string,
+    tip?:string,
+    emoji?:string,
+
+}
+
+
+
+
 export default function Page(){
     const [valor, setValor] = useState<number>(0);
 
@@ -13,13 +24,120 @@ export default function Page(){
         setValor(valor+1);
     }
 
+
+    function Cards_Help({tip, emoji}:prop){
+        return(
+            <div>
+                <button className="cursor-pointer rounded-2xl border p-4">
+                    <figure>
+                        <p className="text-5xl">{emoji}</p>
+                    </figure>
+                    <header className="text-center py-3">
+                        <h1>{tip}</h1>
+                    </header>
+                </button>
+            </div>
+
+        );
+    }
+
     
+    function Cards_Answer({possible_answer, number}:prop){
+        return(
+            <div className="flex">
+
+                <figure className="px-4">
+                    <img className="w-[50px] h-[50px]" src={number} alt="" />
+                </figure>
+
+                <button className="flex bg-amber-700 border rounded p-2 w-full items-center cursor-pointer"> {possible_answer} </button>
+            </div>
+        );
+    }
+
 
     return(
-        <main className="max-w-[1280px] bg-red-200">
-            <header>
-                <h1>Conteudo</h1>
-            </header>
+        <main className="flex w-screen h-screen bg-linear-to-b from-blue-800 to-red-400 items-ceter justify-center">
+            <section className="flex flex-col  w-[1280px] p-5 m-auto gap-y-5 ">
+
+                <section className="flex">
+
+                    <header className="flex-[8] bg-amber-500 text-center rounded-md border p-3 text-2xl font-semibold">
+                        <h1>Conteudo</h1>
+                    </header>
+
+                    <section className="flex-[2]">
+                        <section className="flex w-full h-full justify-center items-center">
+                            <button className=" cursor-pointer border p-4 rounded-md">PARAR</button>
+                        </section>
+                    </section>
+
+                </section>
+
+
+                <section className="flex">
+
+                    <section className="flex-[7]">
+                        <section className="flex flex-col gap-5">
+                            <Cards_Answer
+                            possible_answer = "opção 1"
+                            number="/numbers/numero-1.png"/>
+
+                            <Cards_Answer
+                            possible_answer = "opção 2"
+                            number="/numbers/numero-2.png"/>
+
+                            <Cards_Answer
+                            possible_answer = "opção 3"
+                            number="/numbers/numero-3.png"/>
+
+                            <Cards_Answer
+                            possible_answer = "opção 4"
+                            number="/numbers/numero-4.png"/>
+
+                            <Cards_Answer
+                            possible_answer = "opção 5"
+                            number="/numbers/numero-5.png"/>
+                        </section>
+                    </section>
+
+                    <section className="flex-[3]">
+                        
+                        <div className="flex w-full h-full  justify-center items-center">
+                            
+                            <div className="flex flex-col gap-5 p-4 rounded-2xlm  ">
+                                
+                                <section className="flex flex-col gap-2 bg-blue-500 border rounded-2xl p-6 justify-center items-center">
+
+                                    <header>
+                                        <h1 className="text-2xl text-center">AJUDA</h1>
+                                    </header>
+
+                                    <section className="flex flex-wrap gap-5">
+                                        <Cards_Help
+                                        tip = "Cartas"
+                                        emoji = "🃏"/>
+
+                                        <Cards_Help
+                                        tip = "Cartas"
+                                        emoji = "✂️"/>
+                                    </section>
+                                </section>
+
+                                <section  className="flex m-auto">
+                                    <button className="border rounded-md p-3">PROXIMO</button>
+                                </section>
+                            
+                            </div>
+                        
+                        </div>
+                    </section>
+
+
+
+
+                </section>
+            </section>
 
         </main>
     );
